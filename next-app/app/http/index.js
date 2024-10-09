c20512d5e7a5155d87414889adb7f83f6a6db67a
Dmitry Shevtsov <dshevtsov20@gmail.com>
2024-10-09 20:44:32 +0300

    disable ssl check

--- next-app/app/http/index.js
+++ next-app/app/http/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import https from "https";
 
 const api = axios.create({
   //withCredentials: true,
@@ -6,6 +7,9 @@ const api = axios.create({
     "Access-Control-Allow-Credentials": true,
   },
   baseURL: process.env.API_URL,
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false, // Отключает проверку SSL
+  }),
 });
 
 export default api;
