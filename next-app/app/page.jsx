ddcc57818b20856338b8bd591908b7a68ec0893c
Dmitry Shevtsov <dshevtsov20@gmail.com>
2024-10-09 12:42:59 +0300

    add map section

--- next-app/app/page.jsx
+++ next-app/app/page.jsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero/Hero";
+import Map from "./components/Map/Map";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Map />
     </main>
   );
 }
